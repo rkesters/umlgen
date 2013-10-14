39aae9b098deed4304a8e884579c68e48b66e1be
rkesters <rkesters@gmail.com>
2013-10-14 16:39:04 -0400

    path arg

--- umlgen.py
+++ umlgen.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python
+
+import argparse
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create Sequence')
+    parser.add_argument('path', type=str , help="path to source directory")
+    args = parser.parse_args()
+    print args.path
